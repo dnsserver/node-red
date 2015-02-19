1c03b93eaac4bdc40758dca6de007317a6187fe1
Denis Babani <dbabani@mit.edu>
2015-02-19 16:08:34 -0500

    additional changes to flows

--- nodes/custom/lower-case.js
+++ nodes/custom/lower-case.js
@@ -1,21 +1,32 @@
 module.exports = function(RED){
+  "use strict";
+  var util = require("util");
+  var vm = require("vm");
+
   function LowerCaseNode(config){
+    var sandbox = {
+        console:console,
+        util:util,
+        Buffer:Buffer,
+        context: {
+            global:RED.settings.functionGlobalContext || {}
+        }
+    };
+    var context = vm.createContext(sandbox);
     RED.nodes.createNode(this,config);
     var node = this;
-    console.log(node);
-    console.log(config);
-    this.on('input', function(msg,context){
-      console.log(msg);
-      console.log(context);
+    node.nr_msg = node.nr_msg || 0;
+    this.on('input', function(msg){
+      node.nr_msg ++;
       if(msg.payload.test){
-        node.status({fill:"red",shape:"ring",text:"msgs: "});
+        node.status({fill:"grey",shape:"dot",text:"msgs: "+node.nr_msg});
         msg.payload.test = msg.payload.test.toLowerCase();
       }
+      msg.payload.index = node.nr_msg;
       node.send(msg);
     });
 
     this.on('close', function(done){
-      // tidy up any state
       done();
     });
   }
